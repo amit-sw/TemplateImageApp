193ad1bf0a714c48846167024cd965c6d8c0b2a6
Amit Gupta <amit@gprof.com>
2022-03-07 16:26:58 -0800

    Initial Commit

---
+++ TemplateImageApp/TemplateImageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateImageAppApp.swift
+//  TemplateImageApp
+//
+//  Created by Amit Gupta on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TemplateImageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
